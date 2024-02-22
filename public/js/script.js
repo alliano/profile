2f14880166690a4afc4e02ad887c389aa95ae988
alliano <allianoanonymous@gmail.com>
2024-02-22 14:55:20 +0900

    add fix

--- public/js/script.js
+++ public/js/script.js
@@ -2,8 +2,12 @@
 const myName = "Im, Audia Naila Safa";
 
 document.addEventListener('DOMContentLoaded', function() {
-    new TypeIt("#my-name", {
+    const instance = new TypeIt("#my-name", {
        speed: 50
+    }).type(myName, {
+        delay: 2000
+    }).delete(20, {
+        delay: 2000
     }).type(myName, {
         delay: 2000
     }).delete(20, {
